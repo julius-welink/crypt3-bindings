446d2df8b01c7c7e07fc10fb68adce431181f2c3
Julius Žaromskis <julius@welink.com>
2023-02-15 18:45:57 +0200

    Improve salt quality

--- index.js
+++ index.js
@@ -2,11 +2,28 @@
 const bindings = require('node-gyp-build')(__dirname)
 const crypto = require('crypto');
 
+const SALT = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz./';
+
+function generateSalt(length = 8) {
+  let salt = "";
+  const randomness = crypto.randomBytes(length);
+  for (const byte of randomness) {
+    salt += SALT[byte % SALT.length];
+  }
+  return salt;
+}
+
 module.exports = {
   encrypt(passwd, salt) {
     if (!salt) {
-      salt = crypto.randomBytes(8).toString('hex');
+      salt = generateSalt();
     }
-    return bindings.hashPassword(passwd, salt, 1)
+    const result = bindings.hashPassword(passwd, salt, 1);
+
+    if (!result.startsWith("$1$")) {
+      throw new Error("could not generate hash (unsupported os?)");
+    }
+
+    return result;
   }
 }
